b3bf99bf239b866b401e34ca04a93bf288da0fa7
reshxs <andr.emeljanov2014@gmail.com>
2022-05-02 19:55:07 +0500

    chore(deploy): Запуск миграций из контейнера при старте

--- src/run.py
+++ src/run.py
@@ -32,14 +32,23 @@ def cli():
     default=True,
     help='Enable/Disable debug and auto-reload'
 )
-def web(collectstatic: bool, uvicorn_debug: bool):
+@click.option(
+    '--migrate/--no-migrate',
+    is_flag=True,
+    default=False,
+    help='Apply migrations before run app'
+)
+def web(collectstatic: bool, uvicorn_debug: bool, migrate: bool):
+    if migrate:
+        management.call_command('migrate')
+
+    if collectstatic:
+        management.call_command('collectstatic', '--no-input', '--clear')
+
     app = hr.app.app
 
     if uvicorn_debug:
         app = 'hr.app:app'
-
-    if collectstatic:
-        management.call_command('collectstatic', '--no-input', '--clear')
 
     uvicorn.run(
         app,
