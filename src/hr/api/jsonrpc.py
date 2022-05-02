e482c4bb189761d419415e8ef3e2f3775652006b
reshxs <andr.emeljanov2014@gmail.com>
2022-05-02 15:50:56 +0500

    feat(api): опубликовать резюме

--- src/hr/api/jsonrpc.py
+++ src/hr/api/jsonrpc.py
@@ -1,10 +1,12 @@
+from django.db import transaction
+from django.utils import timezone
 from fastapi import Body
 from fastapi import Depends
 from fastapi_jsonrpc import Entrypoint
 
 from hr import models
-from . import schemas
 from . import errors
+from . import schemas
 from .dependencies import UserGetter
 from .dependencies import get_token
 
@@ -61,5 +63,39 @@ def get_resume_for_applicant(
 
     if resume is None:
         raise errors.ResumeNotFound
+
+    return schemas.ResumeForApplicantSchema.from_model(resume)
+
+
+@api_v1.method(
+    tags=['applicant'],
+    summary='Опубликовать резюме',
+)
+def publish_resume(
+    user: models.User = Depends(
+        UserGetter(
+            allowed_roles=[models.UserRole.EMPLOYEE],
+        ),
+    ),
+    resume_id: int = Body(..., title='ID резюме', alias='id')
+) -> schemas.ResumeForApplicantSchema:
+    with transaction.atomic():
+        resume = (
+            models.Resume.objects
+            .select_for_update(
+                of=('self',)
+            )
+            .get_or_none(
+                user_id=user.id,
+                id=resume_id,
+            )
+        )
+
+        if resume is None:
+            raise errors.ResumeNotFound
+
+        resume.state = models.ResumeState.PUBLISHED
+        resume.published_at = timezone.now()
+        resume.save(update_fields=('state', 'published_at'))
 
     return schemas.ResumeForApplicantSchema.from_model(resume)
